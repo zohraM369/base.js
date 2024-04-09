960dcd472658e22a9ef498d045d22ef25ca44157
zohraM369 <chadly_zohra@hotmail.com>
2024-04-09 14:03:55 +0200

    rectangle

--- produit_object.js
+++ produit_object.js
@@ -9,11 +9,11 @@ let produit = {        //declare objet
 produit.gain=produit.prix*produit.nombreVendu
 //console.log(produit)
 produit.valeur_stock = produit.prix*produit.stock
-console.log("nom" , produit.nom, "gain:", produit.gain, "valeur_stock:", produit.valeur_stock)
+console.log("\n nom" , produit.nom, "gain:", produit.gain, "valeur_stock:", produit.valeur_stock)
 produit.cost = 300
 produit.total_cost = produit.cost*(produit.nombreVendu + produit.stock)
 console.log("somme d'achat des produits:" , produit.total_cost)
 let benefice_par_produit= produit.prix - produit.cost
 console.log("benefice par produit:" , benefice_par_produit)
 let pourcentage_benifice = 100*(benefice_par_produit/produit.prix)
-console.log("pourcentage benefice par produit:", pourcentage_benifice+"%", "~" , Math.round(pourcentage_benifice)+"%")
+console.log("pourcentage benefice par produit:", pourcentage_benifice+"%", "~" , Math.round(pourcentage_benifice)+"%" ,"\n")
