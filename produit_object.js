a39cc463d727edc0c6738637712ae12bee4234b7
zohraM369 <chadly_zohra@hotmail.com>
2024-04-09 11:58:07 +0200

    calcule

--- produit_object.js
+++ produit_object.js
@@ -1,6 +1,6 @@
 let produit = {        //declare objet
     nom: "ordinateur",   // affecte propriété  nom
-    prix: 650,           //affecte prix
+    prix: 1000,           //affecte prix
     stock: 10,           // affecte stock
     nombreVendu: 5
 
@@ -10,3 +10,10 @@ produit.gain=produit.prix*produit.nombreVendu
 //console.log(produit)
 produit.valeur_stock = produit.prix*produit.stock
 console.log("nom" , produit.nom, "gain:", produit.gain, "valeur_stock:", produit.valeur_stock)
+produit.cost = 300
+produit.total_cost = produit.cost*(produit.nombreVendu + produit.stock)
+console.log("somme d'achat des produits:" , produit.total_cost)
+let benefice_par_produit= produit.prix - produit.cost
+console.log("benefice par produit:" , benefice_par_produit)
+let pourcentage_benifice = 100*(benefice_par_produit/produit.prix)
+console.log("pourcentage benefice par produit:", pourcentage_benifice+"%", "~" , Math.round(pourcentage_benifice)+"%")
