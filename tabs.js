57f6a68e19a8924b2f6ee1e7e9bb3ff97020ad5d
zohraM369 <chadly_zohra@hotmail.com>
2024-04-09 15:46:24 +0200

    variable simple et complex

---
+++ tabs.js
@@ -0,0 +1,13 @@
+let variableSimple1 = 12
+let variableSimple2 = variableSimple1
+variableSimple1=35
+console.log("variableSimple1", variableSimple1)
+console.log("variableSimple2", variableSimple2)
+
+let variableComplexe1 = ["informatique", "sport", "television"]
+let variableComplexe2 = variableComplexe1
+let varibleComplexe3 = [...variableComplexe1]
+variableComplexe2.push("course")
+console.log("variableComplexe1", variableComplexe1)
+console.log("variableComplexe2", variableComplexe2)
+console.log("variableComplexe3", variableComplexe3)
